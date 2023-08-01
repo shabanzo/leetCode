8cf46d1ecc2b8cc748fa6ef8a41534efeb6d6c6e
Muhamad Rizky Sya'ban <rizkyshaban@gmail.com>
2023-08-01 23:13:21 +0700

    Time: 53 ms (99.86%), Space: 212.2 MB (11%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.rb
@@ -0,0 +1,15 @@
+def two_sum(nums, target)
+  num_to_index = {}
+
+  nums.each_with_index do |num, i|
+    difference = target - num
+
+    if num_to_index.key?(difference)
+      return [num_to_index[difference], i]
+    end
+
+    num_to_index[num] = i
+  end
+
+  return []
+end
